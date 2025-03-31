a9142276825e89b8dc259f491bb952d09af7b262
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-03-31 20:08:06 +0530

    pause.py

--- VIPMUSIC/plugins/admins/pause.py
+++ VIPMUSIC/plugins/admins/pause.py
@@ -7,7 +7,7 @@ from VIPMUSIC.utils.decorators import AdminRightsCheck
 from VIPMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
-@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["xpause", "xcpause"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
