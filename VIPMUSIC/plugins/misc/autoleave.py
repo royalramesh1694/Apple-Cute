97785235558eae18d884ca3bc102aa724c3b7015
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-04-17 20:57:29 +0530

    autoleave.py

--- VIPMUSIC/plugins/misc/autoleave.py
+++ VIPMUSIC/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002000705279
-                                and i.chat.id != -1002000705279
+                                and i.chat.id != -1001735663878
+                                and i.chat.id != -1001735663878
                             ):
                                 if left == 20:
                                     continue
