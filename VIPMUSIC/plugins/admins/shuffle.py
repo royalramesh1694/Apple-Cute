4850e2f3289672aae3cfcff8f952b0745f3aad18
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-05-07 15:50:13 +0530

    shuffle.py

--- VIPMUSIC/plugins/admins/shuffle.py
+++ VIPMUSIC/plugins/admins/shuffle.py
@@ -11,7 +11,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["shuffle", "cshuffle"]) & filters.group & ~BANNED_USERS
+    filters.command(["xshuffle", "xcshuffle"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def admins(Client, message: Message, _, chat_id):
